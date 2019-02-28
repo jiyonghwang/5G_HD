f61e87ac8965f4f7fc20ecce3400492825fcbc60
JY.Hwang <wldyd1018@naver.com>
2019-02-28 13:40:20 +0900

    ca_kt_4 v1.4

--- HD/ca_kt_1.py
+++ HD/ca_kt_1.py
@@ -1,17 +1,15 @@
-# ca_kt v1.1 measurement add repeat of time
+# ca_kt v1.4 CA_KT final
 
 import time
 import csv
 from datetime import datetime, timedelta
 import datetime
-import test5_1
 import glob
 import serial
+import platform
 import sys
-
 global ser
 from decimal import Decimal
-
 
 class OC():
     def serial_ports(self):
@@ -32,13 +30,13 @@ class OC():
                 result.append(port)
             except (OSError, serial.SerialException):
                 pass
-        ports = [s for s in result if "usbserial-A907RK9V" in s]
+        ports = [s for s in result if "COM12" in s]  #window COM@   IOS usbserial-A907RK9V
         return ports
 
     def ca_init(self):
         ports_l = self.serial_ports()
         for port in ports_l:
-            seri = serial.Serial(port, baudrate=38400, bytesize=7, stopbits=2, parity='E')
+            seri = serial.Serial(port, baudrate = 38400, bytesize = 7, stopbits = 2, parity = 'E')
             seri.write("COM,1\r\n")
             out = ''
             time.sleep(0.2)
@@ -54,7 +52,7 @@ class OC():
             ser.write("MES\r\n")
             out = ''
             time.sleep(0.11)
-            # print(ser)
+            #print(ser)
             while ser.inWaiting() > 0:
                 out += ser.read(1)
             if out != '':
@@ -67,124 +65,283 @@ class OC():
                     x_test = Decimal(x) / 10000
                     y_test = Decimal(y) / 10000
                     lv_test = Decimal(lv)
-                    # print("0 X: %s / Y: %s / Lv: %s" % (x_test, y_test, lv_test))
+                    #print("0 X: %s / Y: %s / Lv: %s" % (x_test, y_test, lv_test))
                     return [x_test, y_test, lv_test]
             elif i == 3:
                 print("CA-310 Read Error")
                 x_test = Decimal(0)
                 y_test = Decimal(0)
                 lv_test = Decimal(0)
-                # print("1 X: %s / Y: %s / Lv: %s" % (x_test, y_test, lv_test))
+                #print("1 X: %s / Y: %s / Lv: %s" % (x_test, y_test, lv_test))
                 return [x_test, y_test, lv_test]
 
+class KT():
+    def serial_ports(self):
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
 
-def time_run(delay, repeat):
-    oc = OC()
-    ser = oc.ca_init()
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        ports = [s for s in result if "COM11" in s] #window COM@   IOS usbserial-A907RK9V
+        return ports
 
-    kt = test5_1
-    serk = kt.KT().keithley_init()
+    def keithley_init(self):
+        ports_l = self.serial_ports()
+        for port in ports_l:
+            seri = serial.Serial(port, baudrate = 19200, bytesize = 8, stopbits = 1, parity = 'N')
+            time.sleep(0.2)
+            print('Keithley OK')
+            return seri
+        print('Connection Fail')
 
-    f = open('time_meas.csv', 'w')
-    wr = csv.writer(f)
-    wr.writerow(["repeat", "Date", "Time", "x", "y", "lv", "value", "units"])
+    def open_serial(self, ser):
+        ser.open()
 
-    VI = kt.KT().measure(serk, 2)
-    if VI[1] != 'VDC':  # signal miss overlap just PC com #DCI:current(ADC) DCV:voltage(VDC)
-        kt.KT().set_system_key(serk, 'DCV')
-        kt.KT().measure(serk, 2)
+    def close_serial(self, ser):
+        ser.close()
 
-    print('stability_Start')
+    def read(self, ser):
+        out = []
+        terminated = False
+        #print(ser)
+        while ser.inWaiting() > 0 or not terminated:
+            data = str(ser.read(1))
+            if data == '\r':
+                out.append('\n')
+                terminated = True
+            else:
+                out.append(data)
+        #print("2",out)
+        if out != '':
+            return ' '.join(out)
+        else:
+            return ''
 
-    while True:
-        now = datetime.datetime.now()
-        nowDate = now.strftime('%Y-%m-%d')
-        repeat1 = repeat
-        while repeat > 0:
-            Vtime = now + timedelta(seconds=delay)
-            while True:
-                now = datetime.datetime.now()
+    def write_and_read(self, ser, command, pause = .25):
+        ser.write(command + '\r')
+        time.sleep(pause)
+        return self.read(ser)
 
-                if repeat == repeat1:
+    def get_response(self, ser, command, pause = .25):
+        if ser.isOpen():
+            response = self.write_and_read(ser, command, pause = pause)
+            if response != '':
+                return response
+            else:
+                return None
+        else:
+            print("Port is closed...")
 
-                    ser.close()
-                    ser.open()
-                    ca_ms = oc.ca_measure(ser)
-                    x = ca_ms[0]
-                    y = ca_ms[1]
-                    lv = ca_ms[2]
+    def measure(self, ser, command):  # command D/S 414page
+        if command == 1:
+            meas1 = self.get_response(ser, ':FETCh?')  # no math function enabled
+            #print(meas1) #full meas          #D/S 349 page
+            value = meas1[0:30]
+            words_value = value.split()
+            words_sum_value = "".join(words_value)
+            words_f_value = float(words_sum_value)
+            units = meas1[30:36]
+            words_units = units.split()
+            words_sum_units = "".join(words_units)
+            #print(meas1[0:30]) #value
+            #print(meas1[30:36]) #Units : VDC(DC Volts) VAC(AC Volts) ADC(DC Current) AAC(AC Current)
+            #print(meas1[37:64]) #Timestamp
+            #print(meas1[67:88]) #ReadingNumber
+            return [words_f_value, words_sum_units]
+        elif command == 2:
+            meas2 = self.get_response(ser, ':MEASure?')  # one-shot meas mode math function enabled (Auto)
+            #print(meas2)
+            value = meas2[0:30]
+            words_value = value.split()
+            words_sum_value = "".join(words_value)
+            words_f_value = float(words_sum_value)
+            units = meas2[30:36]
+            words_units = units.split()
+            words_sum_units = "".join(words_units)
+            #print(meas2[0:30])  #value
+            # print(meas2[30:36]) #Units
+            # print(meas2[0:1]) #value + -
+            # print(str(meas2[2:4]))  # value + -
+            # if meas2[0:1] == '-':
+            #     print('neg')
+            # print(meas2[37:64]) #Timestamp
+            # print(meas2[67:88]) #ReadingNumber
+            #print([words_sub_value, words_sub_units])
+            return [words_f_value, words_sum_units]
+        else:
+            print("Unknown measure setting!")
+            return None
 
-                    serk.close()
-                    serk.open()
-                    kt_ms = kt.KT().measure(serk, 1)  # raw:1 auto:2
-                    value = kt_ms[0]
-                    units = kt_ms[1]
-                    break
+    def send_command(self, ser, command):
+        if ser.isOpen():
+            response = ser.write(command + '\r')
+            if response != '':
+                return response
+            else:
+                return None
+        else:
+            print("Port is closed...")
 
-                else:
-                    if now == Vtime:
-                        ser.close()
-                        ser.open()
-                        ca_ms = oc.ca_measure(ser)
-                        x = ca_ms[0]
-                        y = ca_ms[1]
-                        lv = ca_ms[2]
+    def set_system_key(self, ser, system_key):           # D/S 355 page
+        if system_key == 'DCV':
+            ser.write(':SYSTem:KEY 2' + '\r')
+        elif system_key == 'ACV':
+            ser.write(':SYSTem:KEY 3' + '\r')
+        elif system_key == 'DCI':
+            ser.write(':SYSTem:KEY 4' + '\r')
+        elif system_key == 'ACI':
+            ser.write(':SYSTem:KEY 5' + '\r')
+        elif system_key == 'ohm2':
+            ser.write(':SYSTem:KEY 6' + '\r')
+        elif system_key == 'ohm4':
+            ser.write(':SYSTem:KEY 7' + '\r')
+        elif system_key == 'FREQ':
+            ser.write(':SYSTem:KEY 8' + '\r')
+        elif system_key == 'TEMP':
+            ser.write(':SYSTem:KEY 16' + '\r')
+        elif system_key == 'RANGEUP':
+            ser.write(':SYSTem:KEY 11' + '\r')
+        elif system_key == 'RANGEDOWN':
+            ser.write(':SYSTem:KEY 13' + '\r')
+        elif system_key == 'AUTO':
+            ser.write(':SYSTem:KEY 12' + '\r')
+        elif system_key == 'RATE':
+            ser.write(':SYSTem:KEY 31' + '\r')
+        else:
+            print("Unknown sensor setting!")
+            return None
 
-                        serk.close()
-                        serk.open()
-                        kt_ms = kt.KT().measure(serk, 1)  # raw:1 auto:2
-                        units = kt_ms[1]
-                        value = kt_ms[0]
+class HD():
+    def time_run(self,delay, repeat, mode = None): #mode 0:CA 1:KT 2:CA_KT
+
+        if mode == 0:
+            oc = OC()
+            ser = oc.ca_init()
+        elif mode == 1:
+            serk = KT().keithley_init()
+        elif mode == 2:
+            oc = OC()
+            ser = oc.ca_init()
+            serk = KT().keithley_init()
+
+        f = open('time_meas.csv', 'wb')
+        wr = csv.writer(f)
+
+        if mode == 0:
+            wr.writerow(["repeat", "Date", "Time", "x", "y", "lv"])
+        elif mode == 1:
+            wr.writerow(["repeat", "Date", "Time", "value", "units"])
+        elif mode == 2:
+            wr.writerow(["repeat", "Date", "Time", "x", "y", "lv", "value", "units"])
+
+        if mode != 0:
+            VI = KT().measure(serk, 2)
+            if VI[1] != 'ADC':             #signal miss overlap just PC com #DCI:current(ADC) DCV:voltage(VDC)
+                KT().set_system_key(serk, 'DCI')
+                KT().measure(serk, 2)
+
+        print('stability_Start')
+
+        while True:
+            now = datetime.datetime.now()
+            nowDate = now.strftime('%Y-%m-%d')
+            repeat1 = repeat
+            while repeat > 0:
+                Vtime = now + timedelta(seconds = delay)
+                while True:
+                    now = datetime.datetime.now()
+
+                    if repeat == repeat1:
+                        if mode == 0:
+                            ser.close()
+                            ser.open()
+                            ca_ms = oc.ca_measure(ser)
+                            x = ca_ms[0]
+                            y = ca_ms[1]
+                            lv = ca_ms[2]
+
+                        elif mode == 1:
+
+                            serk.close()
+                            serk.open()
+                            kt_ms = KT().measure(serk, 1)  # raw:1 auto:2
+                            value = kt_ms[0]*1000
+                            units = kt_ms[1]
+
+                        elif mode == 2:
+
+                            ca_ms = oc.ca_measure(ser)
+                            x = ca_ms[0]
+                            y = ca_ms[1]
+                            lv = ca_ms[2]
+                            ser.close()
+
+                            kt_ms = KT().measure(serk,1) # raw:1 auto:2
+                            value = kt_ms[0]*1000
+                            units = kt_ms[1]
                         break
-            nowTime = now.strftime('%H:%M:%S.%f')[:-3]
-            print("repeat:%s Date:%s Time:%s X:%s Y:%s lv:%s val:%s units:%s " % (
-            repeat, nowDate, nowTime, x, y, lv, value, units))
-            wr.writerow([repeat, nowDate, nowTime, x, y, lv, value, units])
-            repeat -= 1
-        break
-    print('stability_completed')
 
+                    elif now >= Vtime:
+                        if mode == 0:
+                            ser.close()
+                            ser.open()
+                            ca_ms = oc.ca_measure(ser)
+                            x = ca_ms[0]
+                            y = ca_ms[1]
+                            lv = ca_ms[2]
 
-def ca_meas(delay, repeat):
-    oc = OC()
-    ser = oc.ca_init()
-    print(ser)
-    while repeat > 0:
-        time.sleep(delay)  # here delay measure problum
-        now = datetime.datetime.now()
-        nowDate = now.strftime('%Y-%m-%d')
-        nowTime = now.strftime('%H:%M:%S')
-        ca_ms = oc.ca_measure(ser)
-        x = ca_ms[0]
-        y = ca_ms[1]
-        lv = ca_ms[2]
-        print("X:%s, Y:%s lv:%s" % (x, y, lv))
-        repeat -= 1
-    print('_completed')
+                        elif mode == 1:
 
+                            serk.close()
+                            serk.open()
+                            kt_ms = KT().measure(serk, 1)  # raw:1 auto:2
+                            value = kt_ms[0]*1000
+                            units = kt_ms[1]
 
-def kt_meas(delay, repeat):
-    kt = test5_1
-    serk = kt.KT().keithley_init()
-    print(serk)
-    while repeat > 0:
-        time.sleep(delay)  # here delay measure problum
-        now = datetime.datetime.now()
-        nowDate = now.strftime('%Y-%m-%d')
-        nowTime = now.strftime('%H:%M:%S')
-        kt_ms = kt.KT().measure(serk, 1)
-        value = kt_ms[0]
-        units = kt_ms[1]
-        print("value:%s, units:%s " % (value, units))
-        repeat -= 1
-    print('_completed')
+                        elif mode == 2:
+                            ser.close()
+                            ser.open()
+                            ca_ms = oc.ca_measure(ser)
+                            x = ca_ms[0]
+                            y = ca_ms[1]
+                            lv = ca_ms[2]
 
+                            serk.close()
+                            serk.open()
+                            kt_ms = KT().measure(serk, 1)  # raw:1 auto:2
+                            value = kt_ms[0]*1000
+                            units = kt_ms[1]
+                        break
+                nowTime = now.strftime('%H:%M:%S.%f')[:-3]
+                if mode == 0:
+                    print("repeat:%s Date:%s Time:%s X:%s Y:%s lv:%s" %(repeat,nowDate, nowTime, x, y, lv))
+                    wr.writerow([repeat,nowDate, nowTime, x, y, lv])
+                elif mode == 1:
+                    print("repeat:%s Date:%s Time:%s val:%.3f units:%s " %(repeat,nowDate, nowTime, value, units))
+                    wr.writerow([repeat,nowDate, nowTime, value, units])
+                elif mode == 2:
+                    print("repeat:%s Date:%s Time:%s X:%s Y:%s lv:%s val:%.3f units:VDC " %(repeat,nowDate, nowTime, x, y, lv, value, units))
+                    wr.writerow([repeat,nowDate, nowTime, x, y, lv, value, units])
+                repeat -= 1
+            break
+        print('stability_completed')
 
-def set_system_key(system_key):  # D/S 355 page
-    kt = test5_1
-    serk = kt.KT().keithley_init()
-    if system_key != '':
-        kt.KT().set_system_key(serk, system_key)
-    else:
-        print("Unknown system setting!")
-        return None
+    def set_system_key(self,system_key):  # D/S 355 page
+        serk = KT().keithley_init()
+        if system_key != '':
+            KT().set_system_key(serk, system_key)
+        else:
+            print("Unknown system setting!")
+            return None
